0b87f088e99e72034b932b47dad0cdde4f66b6b1
alejograjal <alejandro.grajal.s@gmail.com>
2025-05-23 17:45:35 -0600

    feat: add docker file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,8 +6,6 @@ load_dotenv()
 class Settings:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
-    VECTORSTORE_PATH: str = os.getenv("VECTORSTORE_PATH", "data/vectorstore")
-    QA_CACHE_PATH: str = os.getenv("QA_CACHE_PATH", "data/qa_cache")
     REDIS_HOST: str = os.getenv("REDIS_HOST")
     REDIS_PORT: int = int(os.getenv("REDIS_PORT"))
     REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
