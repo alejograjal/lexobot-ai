db6021b0b9d99bec092e4fb11b95e0f637d62aec
alejograjal <alejandro.grajal.s@gmail.com>
2025-06-16 02:32:06 -0600

    fix: adjust next config

--- frontend-admin/next.config.js
+++ frontend-admin/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_LEXOBOT_URL: process.env.NEXT_PUBLIC_API_LEXOBOT_URL,
+    },
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8000/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_LEXOBOT_URL}/api/:path*`,
             },
         ];
     },
