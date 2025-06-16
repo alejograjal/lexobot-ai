75e46bb2708f662f076a038849d306f35287a658
alejograjal <alejandro.grajal.s@gmail.com>
2025-06-16 02:40:44 -0600

    fix: minimal change on destion url

--- frontend-admin/next.config.js
+++ frontend-admin/next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    env: {
-        NEXT_PUBLIC_API_LEXOBOT_URL: process.env.NEXT_PUBLIC_API_LEXOBOT_URL,
-    },
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_LEXOBOT_URL}/api/:path*`,
+                destination: 'http://187.33.158.74:8000/api/:path*',
             },
         ];
     },
