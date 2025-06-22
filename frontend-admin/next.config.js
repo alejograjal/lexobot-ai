cb559e2daff50b976f37fb415be2b68472bfbaa3
alejograjal <alejandro.grajal.s@gmail.com>
2025-06-22 14:27:57 -0600

    fix: remove unneccesary proxy

--- frontend-admin/next.config.js
+++ frontend-admin/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://187.33.158.74:8000/api/:path*',
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: 'http://187.33.158.74:8000/api/:path*',
+    //         },
+    //     ];
+    // },
 }
 
 module.exports = nextConfig
