274400077f01f88c4b08a43b1867bd1f5727ed0f
alejograjal <alejandro.grajal.s@gmail.com>
2025-06-23 21:44:19 -0600

    fix: include image rendering domain

--- frontend-chat/next.config.ts
+++ frontend-chat/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    domains: ['www.chat.lexobot-ai.com'],
+  },
 };
 
 export default nextConfig;
